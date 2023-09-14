7d76033d0751e4efcf5c14ae95f28d2f8030ee74
Sandip Shah <sandip.shah@zealousys.com>
2023-09-14 14:56:12 +0530

    Temp Commit

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhook"],
+  publicRoutes: ["/api/webhook", "sign-in"],
+  ignoredRoutes: [
+    "/api",
+    "/((?!api|trpc))(_next|.+\..+)(.*)",
+  ],
 });
 
 export const config = {
